39a7e6b2cf9b4e99452c64b4dc6a483b341193cf
alexd <alex.denis1206@gmail.com>
2022-01-12 00:20:55 +0200

    Preprocess fixed

--- processing/preprocess.py
+++ processing/preprocess.py
@@ -48,6 +48,8 @@ def construct_e_and_v(metadata_df):
         .select(*metadata_df.columns,
                 f.array_join(f.col("authors_processed"), delimiter=" ").alias("author_name"))
 
+    # authors_e = authors_e.checkpoint()
+
     authors_e = authors_e.alias("left") \
         .join(authors_e.alias("right"),
               f.col("left.id") == f.col("right.id")) \
@@ -56,13 +58,17 @@ def construct_e_and_v(metadata_df):
                 f.col("left.id").alias("article_id"),
                 f.split(f.col("left.categories"), " ").alias("article_categories"),
                 f.col("left.update_date")) \
-        .where(f.col("src") != f.col("dst")) \
-        .groupBy([f.col("src"), f.col("dst")]) \
-        .agg(f.count(f.col("article_id")).alias("articles_count"),
-             f.collect_list("article_id").alias("articles_ids"),
-             f.collect_list("article_categories").alias("articles_categories"),
-             f.collect_list("update_date").alias("articles_update_date")) \
-        .orderBy("src", ascending=True)
+        .where(f.col("src") != f.col("dst"))
+
+    # authors_e = authors_e.checkpoint()
+
+    # authors_e = authors_e \
+    #     .groupBy([f.col("src"), f.col("dst")]) \
+    #     .agg(f.count(f.col("article_id")).alias("articles_count"),
+    #          f.collect_list("article_id").alias("articles_ids"),
+    #          f.collect_list("article_categories").alias("articles_categories"),
+    #          f.collect_list("update_date").alias("articles_update_date")) \
+    #     .orderBy("src", ascending=True)
     # .orderBy("articles_count", ascending=False)
 
     authors_e.show()
