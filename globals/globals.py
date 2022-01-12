f0a86bd3a977c13fef90738487ca37f41052fcae
alexd <alex.denis1206@gmail.com>
2022-01-12 17:16:35 +0200

    Preprocess fixed

--- globals/globals.py
+++ globals/globals.py
@@ -1,5 +1,5 @@
 LOCAL_DATA_PATH = "../data/original/arxiv-metadata-oai-snapshot.json"
-CLUSTER_DATA_PATH = "file://~/BigDataProject/data/original/arxiv-metadata-oai-snapshot.json"
+CLUSTER_DATA_PATH = "file:///~/BigDataProject/data/original/arxiv-metadata-oai-snapshot.json"
 
 LOCAL_WRITE_AUTHORS_PATH = "../data/authors_graph"
-CLUSTER_WRITE_AUTHORS_PATH = "file://~/BigDataProject/data/authors_graph"
+CLUSTER_WRITE_AUTHORS_PATH = "file:///~/BigDataProject/data/authors_graph"
