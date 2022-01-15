686147ccb56543e35a27c69a22ace26c84be753c
alexd <alex.denis1206@gmail.com>
2022-01-15 12:18:18 +0200

    cluster run

--- louvain/main.py
+++ louvain/main.py
@@ -7,7 +7,7 @@ session = SparkSession \
     .builder \
     .appName("Louvain run") \
     .config("spark.executor.memory", "8g") \
-    .config("spark.executor.cores", "5") \
+    .config("spark.executor.cores", "1") \
     .getOrCreate()
     # .config("spark.driver.memory", "8g") \
 # .config("spark.driver.memory", "4g") \
