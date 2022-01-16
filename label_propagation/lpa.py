9f4b90722c0232f405e778d8b4b40ce2162c0991
alexd <alex.denis1206@gmail.com>
2022-01-16 14:54:41 +0200

    cluster run

--- label_propagation/lpa.py
+++ label_propagation/lpa.py
@@ -1,7 +1,14 @@
 
 def find_communities_in_graph(graph, num_iterations_lpa=2):
-    found_communities = graph.labelPropagation(maxIter=num_iterations_lpa) \
-                             .withColumnRenamed("id", "author").withColumnRenamed("label", "id_community")
+    found_communities = graph.labelPropagation(maxIter=num_iterations_lpa)
+
+    print("Finished0")
+    print("count0 : ", found_communities.count())
+
+    found_communities = found_communities.withColumnRenamed("id", "author").withColumnRenamed("label", "id_community")
+
+    print("Finished1")
+    print("count1 : ", found_communities.count())
 
     found_communities = found_communities.persist()
 
