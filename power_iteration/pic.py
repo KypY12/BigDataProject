6466a77bd02ef382eecbb4f34d350a524c77503a
alexd <alex.denis1206@gmail.com>
2022-01-16 21:56:54 +0200

    Power iteration clustering

--- power_iteration/pic.py
+++ power_iteration/pic.py
@@ -12,6 +12,7 @@ session = SparkSession \
     .appName(f"Power Iteration Clustering run clusters{NUMBER_OF_CLUSTERS_PIC}:max_iter{MAX_ITERATIONS_PIC}") \
     .config("spark.executor.memory", "8g") \
     .config("spark.executor.cores", "5") \
+    .config("spark.driver.memory", "8g") \
     .getOrCreate()
 
 # from processing.preprocess import preprocess_data
