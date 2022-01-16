8ebe4e0916702daec47ab5405dda248ca32c1d83
adamcristi <adamcristi98@yahoo.com>
2022-01-16 13:16:59 +0200

    Update

--- label_propagation/lpa.py
+++ label_propagation/lpa.py
@@ -1,5 +1,5 @@
 
-def find_communities_in_graph(graph, num_iterations_lpa=10):
+def find_communities_in_graph(graph, num_iterations_lpa=5):
     found_communities = graph.labelPropagation(maxIter=num_iterations_lpa) \
                              .withColumnRenamed("id", "author").withColumnRenamed("label", "id_community")
 
