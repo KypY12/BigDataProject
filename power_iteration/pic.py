01df8d6bd220b8a378a99c61d75f17d15bc85a4e
alexd <alex.denis1206@gmail.com>
2022-01-16 21:20:36 +0200

    Power iteration clustering

---
+++ power_iteration/pic.py
@@ -0,0 +1,81 @@
+from pyspark.ml.clustering import PowerIterationClustering
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as f
+
+from component_size_analysis.create_connected_components import get_saved_connected_component_subgraph
+
+NUMBER_OF_CLUSTERS_PIC = 100
+MAX_ITERATIONS_PIC = 100
+
+session = SparkSession \
+    .builder \
+    .appName(f"Power Iteration Clustering run clusters{NUMBER_OF_CLUSTERS_PIC}:max_iter{MAX_ITERATIONS_PIC}") \
+    .config("spark.executor.memory", "8g") \
+    .config("spark.executor.cores", "5") \
+    .getOrCreate()
+
+# from processing.preprocess import preprocess_data
+
+if __name__ == '__main__':
+    # sample_size = 100
+    # metadata_df = session.read.json("../data/original/arxiv-metadata-oai-snapshot.json").limit(sample_size)
+    # metadata_df.show()
+    # g = preprocess_data(metadata_df)
+
+    component_id = 1
+
+    g = get_saved_connected_component_subgraph(session, component_id)
+
+    g.vertices.show()
+    g.edges.show()
+
+    # Assing an integer id to vertices
+    vertices_ids = g.vertices.withColumn("node_id", f.monotonically_increasing_id()).persist()
+
+    vertices_ids.show()
+
+    # Replace vertices string id with their int integer id
+    edges_with_ids = g.edges.alias("edges") \
+        .join(vertices_ids.alias("vids1"),
+              f.col("vids1.id") == f.col("edges.src")) \
+        .join(vertices_ids.alias("vids2"),
+              f.col("vids2.id") == f.col("edges.dst")) \
+        .select(f.col("vids1.node_id").alias("src"),
+                f.col("vids2.node_id").alias("dst"),
+                f.col("edges.articles_count")) \
+        .persist()
+
+    edges_with_ids.show()
+
+    # Create PIC model
+    pic = PowerIterationClustering(k=NUMBER_OF_CLUSTERS_PIC,
+                                   maxIter=MAX_ITERATIONS_PIC,
+                                   initMode="degree",
+                                   weightCol="articles_count")
+
+    # Cluster data (find communities in the graph)
+    communities = pic.assignClusters(edges_with_ids)
+
+    # Replace vertices integer id with their initial string id
+    communities = communities.alias("comm") \
+        .join(vertices_ids.alias("vids"),
+              f.col("comm.id") == f.col("vids.node_id")) \
+        .select(f.col("vids.id"),
+                f.col("comm.cluster").alias("community"))\
+        .persist()
+
+    communities.write \
+        .option("header", True) \
+        .mode("overwrite") \
+        .parquet(f"../data/pic_communities_{component_id}_{NUMBER_OF_CLUSTERS_PIC}_{MAX_ITERATIONS_PIC}")
+
+    communities.show()
+    communities.unpersist()
+
+    communities = session.read.parquet(
+        f"../data/pic_communities_{component_id}_{NUMBER_OF_CLUSTERS_PIC}_{MAX_ITERATIONS_PIC}")
+
+    communities.show()
+
+    edges_with_ids.unpersist()
+    vertices_ids.unpersist()
