39b9aedde5cc25333932dba005d830db20531852
adamcristi <adamcristi98@yahoo.com>
2022-01-17 11:41:02 +0200

    Update small world analysis

--- small_world_analysis/analysis_main.py
+++ small_world_analysis/analysis_main.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import numpy as np
+
 from processing.preprocess import preprocess_data, write_coauthorship_graph, read_coauthorship_graph
 from component_size_analysis.create_connected_components import get_saved_connected_component_subgraph
 from small_world_analysis.characteristic_path_length import get_characteristic_path_length
@@ -8,6 +11,34 @@ from pyspark.sql import SparkSession
 from pyspark.sql.types import LongType
 import pyspark.sql.functions as f
 
+from networkx.generators.random_graphs import dense_gnm_random_graph, gnp_random_graph
+
+
+# Generate random graph with same number of authors and connections
+def create_random_graph(num_authors, num_connections):
+
+    graph_generated_random = dense_gnm_random_graph(n=num_authors, m=num_connections)
+
+    vertices_graph_generated_random = np.array(graph_generated_random.nodes)
+    edges_graph_generated_random = np.array(graph_generated_random.edges)
+
+    df_vertices = pd.DataFrame(data=vertices_graph_generated_random, columns=["id"])
+    df_edges = pd.DataFrame(data=edges_graph_generated_random, columns=["src", "dst"])
+
+    vertices_random_graph = session.createDataFrame(data=df_vertices)
+    edges_random_graph = session.createDataFrame(data=df_edges)
+
+    try:
+        import graphframes
+    except:
+        print("Couldn't import graphframes package!")
+
+    r_g = graphframes.GraphFrame(vertices_random_graph, edges_random_graph)
+
+    r_g.persist()
+
+    return r_g
+
 
 if __name__ == "__main__":
     session = SparkSession \
@@ -16,20 +47,30 @@ if __name__ == "__main__":
         .config("spark.executor.memory", "7g") \
         .config("spark.executor.cores", "5") \
         .getOrCreate()
-    # .config("spark.driver.memory", "8g") \
+    # .config("spark.driver.memory", "8g") \]
 
     # Testing Locally
-    sample_size = 15 #500  # 15
+    #sample_size = 15 #500  # 15
     # metadata_df = session.read.json("../data/original/arxiv-metadata-oai-snapshot.json")
-    metadata_df = session.read.json("../data/original/arxiv-metadata-oai-snapshot.json").limit(sample_size)
+    #metadata_df = session.read.json("../data/original/arxiv-metadata-oai-snapshot.json").limit(sample_size)
 
-    component = preprocess_data(metadata_df)
+    #component = preprocess_data(metadata_df)
 
     component_id = 1
-    # component = get_saved_connected_component_subgraph(session, component_id)
+    component = get_saved_connected_component_subgraph(session, component_id)
 
     characteristic_path_length_component = get_characteristic_path_length(component_graph=component)
     print(f"Characteristic Path Length of the Component {component_id}: {characteristic_path_length_component}")
 
     clustering_coefficient_component = get_clustering_coefficient(component_graph=component)
     print(f"Clustering Coefficient of the Component {component_id}: {clustering_coefficient_component}\n")
+
+    # Generate random graph similar to the component
+    random_graph = create_random_graph(num_authors=component.vertices.count(),
+                                       num_connections=component.edges.count())
+
+    characteristic_path_length_random_graph = get_characteristic_path_length(component_graph=random_graph)
+    print(f"Characteristic Path Length of the Graph Random Generated similar to Component {component_id}: {characteristic_path_length_random_graph}")
+
+    clustering_coefficient_random_graph = get_clustering_coefficient(component_graph=random_graph)
+    print(f"Clustering Coefficient of the Graph Random Generated similar to Component {component_id}: {clustering_coefficient_random_graph}\n")
